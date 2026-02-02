434787acbfe587e0e69cc123446ea929fb115d0d
akak35793-oss <akak4456@naver.com>
2026-02-02 17:15:10 +0900

    Admin 페이지 구성함

--- frontend/src/routes/Admin.svelte
+++ frontend/src/routes/Admin.svelte
@@ -1,1 +1,137 @@
-<main>Admin</main>
+<main>
+  <p>
+    Make your own word search game on any topic you like, simply by providing
+    between 10 and 30 words. Once submitted, your puzzle will be instantly
+    playable on-line as well as easily printed, so you can share it with
+    friends. Instructions are available at the bottom of this page
+  </p>
+  <div id="title">
+    <label for="title">Title</label>
+    <input type="text" id="title" />
+  </div>
+  <div id="description">
+    <label for="description">Description</label>
+    <textarea></textarea>
+  </div>
+  <div id="wordlist">
+    <label for="word">Word List</label>
+    <p class="word-list-p">
+      Between 10 and 30 words. Puzzles are randomly generated using a selection
+      of your words at play time.
+    </p>
+    <div id="wordlist-input">
+      <div class="word"><input type="text" id="word" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+      <div class="word"><input type="text" /></div>
+    </div>
+  </div>
+  <button id="submit">Submit</button>
+</main>
+
+<style>
+  main {
+    margin: 36px 60px 18px 60px;
+  }
+  p {
+    color: var(--footer-background);
+    text-align: center;
+    padding-top: 12px;
+    padding-bottom: 30px;
+  }
+  .word-list-p {
+    text-align: left;
+    padding-bottom: 5px;
+  }
+  main label {
+    margin: 18px 0 4px;
+    font-size: 1.2em;
+    font-weight: 700;
+    color: var(--footer-background);
+  }
+  main input,
+  main textarea {
+    box-sizing: border-box;
+    color: var(--input-text-color);
+    background: var(--white-text-color);
+    border: 1px solid var(--gray-color);
+    width: 100%;
+    padding: 6px 8px;
+    font-family: inherit;
+    font-size: 1.2em;
+    font-weight: 700;
+    display: block;
+    margin-top: 8px;
+  }
+  #description {
+    margin-top: 18px;
+  }
+  #wordlist {
+    margin-top: 18px;
+  }
+  #wordlist-input {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wordlist .word {
+    margin: 0;
+  }
+  #wordlist .word input {
+    margin: 0;
+  }
+  @media (min-width: 520px) {
+    #wordlist .word {
+      width: 50%;
+    }
+  }
+  @media (min-width: 800px) {
+    #wordlist .word {
+      width: 33.33%;
+    }
+  }
+  @media (min-width: 1000px) {
+    #wordlist .word {
+      width: 25%;
+    }
+  }
+  @media (min-width: 1250px) {
+    #wordlist .word {
+      width: 20%;
+    }
+  }
+  #submit {
+    color: var(--input-text-color);
+    background-color: var(--submit-background);
+    margin: 30px 0;
+    width: 100%;
+    padding: 18px;
+    font-size: 1.4em;
+    font-weight: 700;
+  }
+</style>
